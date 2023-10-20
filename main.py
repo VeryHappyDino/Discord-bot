9045131ea47d33478355a7e7e8c3aa40005fb648
VeryHappyDino <145197196+VeryHappyDino@users.noreply.github.com>
2023-10-20 23:27:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import discord
 import random
 
@@ -23,8 +24,35 @@ async def on_message(message):
     if message.content.startswith('/numero_casuale'):
         risultato = random.choice(['Uno', 'Due', 'Tre', 'Quattro', 'Cinque'])
         await message.channel.send(f'Hai lanciato un numero casuale ed è uscito: {risultato}')
+        
+    if message.content.startswith("/meme"):
+        
+        names = os.listdir("images")
+        random_image = names.random.choice(names)
+        with open(f"./images/{random_image}","rb") as f:
+            picture = discord.File(f)
+        await message.channel.send(file=picture)
+    if message.content.startswith("/memes animals"):
+        names2 = os.listdir("memesanimals")
+        random_animalmeme = names2.random.choice(names2)
+        with open(f"./memesanimals/{random_animalmeme}","rb") as f:
+            picture = discord.file(f)
+        await message.channed.send(file=picture)
+    if message.content.startswith("/memes_school"):
+        names3 = os.listdir("memeschool")
+        random_schoolmeme = names3.random.choice(names3)
+        with open(f"./memeschool/{random_schoolmeme}","rb") as f:
+            picture = discord.file(f)
+        await message.channed.send(file=picture)
+    if message.content.startswith("/weird_thought"):
+        names4 = os.listdir("weird_thought")
+        random_weirdthoughtmeme = names4.random.choice(names4)
+        with open(f"./weird_thoughts/{random_weirdthoughtmeme}","rb") as f:
+            picture = discord.file(f)
+        await message.channed.send(file=picture)
+
 
 # Inserisci il tuo token del bot Discord qui
-TOKEN = 'MTE1NzYxODk4NTc5NDI4NTYxOA.G9Tow9._xdzxCG6TmSfLu_sDylmDlmBOuWzGKC4oShruA'
+TOKEN = 'MTE1NzYxODk4NTc5NDI4NTYxOA.GZyQa-.5mPb_8ifXOmkKTZtLfnqz5EH0HKYmFWY7Gy688'
 
 client.run(TOKEN)
